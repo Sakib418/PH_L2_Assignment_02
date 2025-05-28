ad729dcbb88319d228770c395f4c7aea4562c723
Sakib418 <ifthekerskb@gmail.com>
2025-05-29 01:52:30 +0600

    All task done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,4 @@
--- Active: 1748327139947@@127.0.0.1@5432@conservation_db@public
+-- Active: 1748457571027@@127.0.0.1@5432@conservation_db@public
 --Database creation
 CREATE DATABASE conservation_db;
 
@@ -52,4 +52,40 @@ INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VA
 (1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
 
-Select * from sightings;
+--Problem 01
+Insert into rangers(name,region)
+VALUES('Derek Fox','Coastal Plains')
+
+--Problem 02
+Select count(DISTINCT species_id) AS unique_species_count from sightings;
+
+
+--Problem 03
+
+Select * from sightings WHERE LOCATION ILIKE '%Pass%';
+
+--Problem 04
+Select name,count(B.sighting_id) total_sightings from rangers as A JOIN sightings AS B ON A.ranger_id = B.ranger_id GROUP BY A.name;
+
+--Problem 05
+SELECT common_name from species where species_id not in (Select species_id from sightings);
+
+--Problem 06
+Select c.common_name,sighting_time,b.name from sightings as a join rangers as b on a.ranger_id = b.ranger_id join species as c on a.species_id = c.species_id ORDER BY sighting_time ASC LIMIT 2;
+
+--Problem 07
+UPDATE species SET conservation_status = 'Historic' WHERE extract(YEAR FROM discovery_date) < 1800;
+
+--Problem 08
+Select sighting_id,CASE
+    WHEN extract(HOUR from sighting_time) < 12 THEN 'Morning' 
+    WHEN extract(HOUR from sighting_time) >= 12 AND extract(HOUR from sighting_time) <= 16 THEN 'Afternoon'
+    when extract(HOUR from sighting_time) > 16 AND extract(HOUR from sighting_time) <= 24 THEN 'Evening' 
+END time_of_day from sightings;
+
+--Problem 09
+Delete FROM rangers where ranger_id not in (Select DISTINCT ranger_id from sightings);
+
+
+
+ 
